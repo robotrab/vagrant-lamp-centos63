737ff18d0548d6b3917f757b68133ce79804b232
Robert Brown <rabrown.mail@gmail.com>
2013-04-30 09:47:09 -0400

    Added gittest. Updated vhosts.

--- vhosts.rb
+++ vhosts.rb
@@ -15,6 +15,6 @@ parsed["domains"].each do |domain|
   File.open("./files/httpd/vhosts/#{domain['full_domain']}.conf", "w") do |f|
     f.write(domain_template.result(binding))
   end
-  FileUtils.mkdir "./clients/#{folder_name}"
+  FileUtils.mkdir "./clients/#{folder_name}" unless File.directory?("./clients/#{folder_name}")
 end
 
