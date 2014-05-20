53c8f5fdeddbbf23d542e658c6ee92419ab2de6d
Robert Brown <netadmin@fuelinteractive.com>
2014-05-20 16:37:14 -0400

    bluewavenc.net site

--- files/mysql/create_dbs.sh
+++ files/mysql/create_dbs.sh
@@ -1,62 +1,64 @@
-/usr/bin/mysql -uroot -proot -e "create database Local-CourseSites; grant all on Local-CourseSites.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MbGolfCourseSites; grant all on Local-MbGolfCourseSites.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Mbga; grant all on Local-Mbga.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MBN; grant all on Local-MBN.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MbGolf; grant all on Local-MbGolf.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MbGolfTrips; grant all on Local-MbGolfTrips.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MbHotels; grant all on Local-MbHotels.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-BrmGlobal; grant all on Local-BrmGlobal.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-AppVMB; grant all on Local-AppVMB.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-BeaufortSC; grant all on Local-BeaufortSC.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-BobbyChapman; grant all on Local-BobbyChapman.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-CalcuttaBaits; grant all on Local-CalcuttaBaits.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-CampMyrtleBeach; grant all on Local-CampMyrtleBeach.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Century21Boling; grant all on Local-Century21Boling.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-ConwayTiger; grant all on Local-ConwayTiger.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-ColumbusRegional; grant all on Local-ColumbusRegional.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Driwood; grant all on Local-Driwood.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-EEbase; grant all on Local-EEbase.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-EliteLighting; grant all on Local-EliteLighting.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-ElmsDigestivesc; grant all on Local-ElmsDigestivesc.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-FtcNow; grant all on Local-FtcNow.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Fuel; grant all on Local-Fuel.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-GloryFoods; grant all on Local-GloryFoods.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-GrandStrand; grant all on Local-GrandStrand.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-HaveaNiceBank; grant all on Local-HaveaNiceBank.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-HolidaySandsSouth; grant all on Local-HolidaySandsSouth.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-HtcAdsales; grant all on Local-HtcAdsales.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-IgaGuy; grant all on Local-IgaGuy.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-InmanMills; grant all on Local-InmanMills.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-InmanSod; grant all on Local-InmanSod.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-ManzerBasketball; grant all on Local-ManzerBasketball.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MarionCounty; grant all on Local-MarionCounty.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MyMyrtlebeachResorts; grant all on Local-MyMyrtlebeachResorts.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MyrtleBeachResorts; grant all on Local-MyrtleBeachResorts.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Mbgp; grant all on Local-Mbgp.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-TeeTimes; grant all on Local-TeeTimes.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-NewSouth; grant all on Local-NewSouth.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-CapeFear; grant all on Local-CapeFear.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-TammyErwin; grant all on Local-TammyErwin.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-PlantationShutter; grant all on Local-PlantationShutter.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-GolfVacations; grant all on Local-GolfVacations.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-WiplConference; grant all on Local-WiplConference.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MyrtleBeach; grant all on Local-MyrtleBeach.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MbIFG; grant all on Local-MbIFG.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Oldmb; grant all on Local-Oldmb.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Pawleys; grant all on Local-Pawleys.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-vmbnew; grant all on Local-vmbnew.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-APST; grant all on Local-APST.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-OxfordGolf; grant all on Local-OxfordGolf.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-SouthernTide; grant all on Local-SouthernTide.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-WilliamsKnife; grant all on Local-WilliamsKnife.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MyrtleBeachNet; grant all on Local-MyrtleBeachNet.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-RepowerSouth; grant all on Local-RepowerSouth.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MBCondoRentals; grant all on Local-MBCondoRentals.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-Guestmate; grant all on Local-Guestmate.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-TheBrandonAgency; grant all on Local-TheBrandonAgency.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-GolfHoliday; grant all on Local-GolfHoliday.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-TilghmanResort; grant all on Local-TilghmanResort.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-TeeUpTomorrow; grant all on Local-TeeUpTomorrow.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-MBReviews; grant all on Local-MBReviews.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-NorthMBHotels; grant all on Local-NorthMBHotels.* to fueldev@localhost identified by 'fueldev';"
-/usr/bin/mysql -uroot -proot -e "create database Local-ExploreNorthMyrtleBeach; grant all on Local-ExploreNorthMyrtleBeach.* to fueldev@localhost identified by 'fueldev';"
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-CourseSites\`; grant all on \`Local-CourseSites\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbGolfCourseSites\`; grant all on \`Local-MbGolfCourseSites\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Mbga\`; grant all on \`Local-Mbga\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MBN\`; grant all on \`Local-MBN\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbGolf\`; grant all on \`Local-MbGolf\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbGolfTrips\`; grant all on \`Local-MbGolfTrips\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbHotels\`; grant all on \`Local-MbHotels\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-BrmGlobal\`; grant all on \`Local-BrmGlobal\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-AppVMB\`; grant all on \`Local-AppVMB\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-BeaufortSC\`; grant all on \`Local-BeaufortSC\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-BobbyChapman\`; grant all on \`Local-BobbyChapman\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-CalcuttaBaits\`; grant all on \`Local-CalcuttaBaits\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-CampMyrtleBeach\`; grant all on \`Local-CampMyrtleBeach\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Century21Boling\`; grant all on \`Local-Century21Boling\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ConwayTiger\`; grant all on \`Local-ConwayTiger\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ColumbusRegional\`; grant all on \`Local-ColumbusRegional\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Driwood\`; grant all on \`Local-Driwood\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-EEbase\`; grant all on \`Local-EEbase\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-EliteLighting\`; grant all on \`Local-EliteLighting\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ElmsDigestivesc\`; grant all on \`Local-ElmsDigestivesc\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-FtcNow\`; grant all on \`Local-FtcNow\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Fuel\`; grant all on \`Local-Fuel\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-GloryFoods\`; grant all on \`Local-GloryFoods\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-GrandStrand\`; grant all on \`Local-GrandStrand\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-HaveaNiceBank\`; grant all on \`Local-HaveaNiceBank\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-HolidaySandsSouth\`; grant all on \`Local-HolidaySandsSouth\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-HtcAdsales\`; grant all on \`Local-HtcAdsales\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-IgaGuy\`; grant all on \`Local-IgaGuy\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-InmanMills\`; grant all on \`Local-InmanMills\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-InmanSod\`; grant all on \`Local-InmanSod\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ManzerBasketball\`; grant all on \`Local-ManzerBasketball\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MarionCounty\`; grant all on \`Local-MarionCounty\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MyMyrtlebeachResorts\`; grant all on \`Local-MyMyrtlebeachResorts\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MyrtleBeachResorts\`; grant all on \`Local-MyrtleBeachResorts\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Mbgp\`; grant all on \`Local-Mbgp\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-TeeTimes\`; grant all on \`Local-TeeTimes\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-NewSouth\`; grant all on \`Local-NewSouth\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-CapeFear\`; grant all on \`Local-CapeFear\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-TammyErwin\`; grant all on \`Local-TammyErwin\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-PlantationShutter\`; grant all on \`Local-PlantationShutter\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-GolfVacations\`; grant all on \`Local-GolfVacations\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-WiplConference\`; grant all on \`Local-WiplConference\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MyrtleBeach\`; grant all on \`Local-MyrtleBeach\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbIFG\`; grant all on \`Local-MbIFG\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Oldmb\`; grant all on \`Local-Oldmb\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Pawleys\`; grant all on \`Local-Pawleys\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-vmbnew\`; grant all on \`Local-vmbnew\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-APST\`; grant all on \`Local-APST\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-OxfordGolf\`; grant all on \`Local-OxfordGolf\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-SouthernTide\`; grant all on \`Local-SouthernTide\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-WilliamsKnife\`; grant all on \`Local-WilliamsKnife\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MyrtleBeachNet\`; grant all on \`Local-MyrtleBeachNet\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-RepowerSouth\`; grant all on \`Local-RepowerSouth\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MBCondoRentals\`; grant all on \`Local-MBCondoRentals\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-Guestmate\`; grant all on \`Local-Guestmate\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-TheBrandonAgency\`; grant all on \`Local-TheBrandonAgency\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-GolfHoliday\`; grant all on \`Local-GolfHoliday\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-TilghmanResort\`; grant all on \`Local-TilghmanResort\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-TeeUpTomorrow\`; grant all on \`Local-TeeUpTomorrow\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MBReviews\`; grant all on \`Local-MBReviews\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-NorthMBHotels\`; grant all on \`Local-NorthMBHotels\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ExploreNorthMyrtleBeach\`; grant all on \`Local-ExploreNorthMyrtleBeach\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MbGolfPassport\`; grant all on \`Local-MbGolfPassport\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-BluewaveNC\`; grant all on \`Local-BluewaveNC\`.* to 'fueldev'@'localhost';";
