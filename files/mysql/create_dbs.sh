aea4dd525c18131b82a1b3bcf30554de7dd8899c
Robert Brown <netadmin@fuelinteractive.com>
2015-01-30 12:09:09 -0500

    myrtlebeachgroupsales.com

--- files/mysql/create_dbs.sh
+++ files/mysql/create_dbs.sh
@@ -70,3 +70,4 @@
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-GuestExpress\`; grant all on \`Local-GuestExpress\`.* to 'fueldev'@'localhost';";
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-ACG\`; grant all on \`Local-ACG\`.* to 'fueldev'@'localhost';";
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-WachesawEast\`; grant all on \`Local-WachesawEast\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-MBGroupSales\`; grant all on \`Local-MBGroupSales\`.* to 'fueldev'@'localhost';";
