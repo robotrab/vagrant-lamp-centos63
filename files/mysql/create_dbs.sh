a042ca8a1b06c4bc4e3a868ff1be9d6cc3a53cce
Robert Brown <netadmin@fuelinteractive.com>
2015-07-17 11:24:46 -0400

    e3-studio.com

--- files/mysql/create_dbs.sh
+++ files/mysql/create_dbs.sh
@@ -76,3 +76,4 @@
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-StrandMyrtleBeach\`; grant all on \`Local-StrandMyrtleBeach\`.* to 'fueldev'@'localhost';";
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-BellamyLaw\`; grant all on \`Local-BellamyLaw\`.* to 'fueldev'@'localhost';";
 /usr/bin/mysql -uroot -pfueldev -e "create database \`Local-HbSpringsInsurance\`; grant all on \`Local-HbSpringsInsurance\`.* to 'fueldev'@'localhost';";
+/usr/bin/mysql -uroot -pfueldev -e "create database \`Local-E3Studio\`; grant all on \`Local-E3Studio\`.* to 'fueldev'@'localhost';";
